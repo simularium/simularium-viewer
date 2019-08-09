7f8940163bff493e9714d3a6e888a8ab355591bc
Megan Riel-Mehan <meganrm@gmail.com>
2019-08-09 16:14:45 -0700

    dev mode

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
         libraryTarget: 'umd',
         path: path.resolve(__dirname, 'dist'),
     },
-    mode: 'production',
+    mode: 'development',
     module: {
         rules: [
             {
