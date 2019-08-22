0a5c423c726d9ae6977cd1bce8d27b3da54f199a
Megan Riel-Mehan <meganrm@gmail.com>
2019-08-22 13:41:58 -0700

    seems like javascript module typing is working.

---
+++ src/agentsim/index.d.ts
@@ -0,0 +1,11 @@
+export declare var VisGeometry: any;
+
+export declare var VisData: any;
+
+export declare var NetConnection: any;
+
+export declare var SimParameters: any;
+
+export declare var DevGUI: any;
+
+export declare var ThreadUtil: any;
--- src/viewport/index.tsx
+++ src/viewport/index.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import jsLogger from 'js-logger';
 
-import { VisGeometry, DevGUI } from "../agentsim";
+import { VisGeometry, DevGUI  }from "../agentsim";
+// const { VisGeometry, DevGUI } = agentsim;
 
 interface AgentSimController {
     // NOTE: these can be typed in the future, but they may change signifantly and I dont want to at the moment. -MMRM
