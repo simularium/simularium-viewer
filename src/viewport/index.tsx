48a0585524633ff48c4844eea7b91f6e97e61732
Eric <eric.j.isaac@gmail.com>
2020-02-20 17:28:42 -0800

    pass color array directly to visGeometry

--- src/viewport/index.tsx
+++ src/viewport/index.tsx
@@ -103,7 +103,43 @@ class Viewport extends React.Component<ViewportProps> {
         this.visGeometry = new VisGeometry(loggerLevel);
         this.animate = this.animate.bind(this);
         this.visGeometry.setupScene();
-        this.visGeometry.createMaterials(agentSimController.visData.colors);
+        this.visGeometry.createMaterials([
+            0x6ac1e5,
+            0xff2200,
+            0xee7967,
+            0xff6600,
+            0xd94d49,
+            0xffaa00,
+            0xffcc00,
+            0x00ccff,
+            0x00aaff,
+            0x8048f3,
+            0x07f4ec,
+            0x79bd8f,
+            0x8800ff,
+            0xaa00ff,
+            0xcc00ff,
+            0xff00cc,
+            0xff00aa,
+            0xff0088,
+            0xff0066,
+            0xff0044,
+            0xff0022,
+            0xff0000,
+            0xccff00,
+            0xaaff00,
+            0x88ff00,
+            0x00ffcc,
+            0x66ff00,
+            0x44ff00,
+            0x22ff00,
+            0x00ffaa,
+            0x00ff88,
+            0x00ffaa,
+            0x00ffff,
+            0x0066ff,
+
+        ]);
         this.visGeometry.createMeshes();
         this.vdomRef = React.createRef();
         this.dispatchUpdatedTime = this.dispatchUpdatedTime.bind(this);
