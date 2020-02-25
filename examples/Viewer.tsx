8de79b5af55fe762ef9f636c6d1670da621980bc
Eric <eric.j.isaac@gmail.com>
2020-02-24 16:24:22 -0800

    clean up example viewer (of temp dev testing)

--- examples/Viewer.tsx
+++ examples/Viewer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import AgentVizViewer, { DummyNetConnection, NetConnection, AgentSimController } from '../dist';
+import AgentVizViewer, { NetConnection, AgentSimController } from '../dist';
 import './style.css';
 import { CLIENT_RENEG_WINDOW } from "tls";
 
@@ -9,8 +9,7 @@ const netConnectionSettings = {
     serverPort: 9002
 }
 
-//const netConn = new NetConnection(netConnectionSettings);
-const netConn = new DummyNetConnection({});
+const netConn = new NetConnection(netConnectionSettings);
 
 interface ViewerState {
     highlightId: number;
@@ -26,11 +25,9 @@ interface ViewerState {
     totalDuration: number;
 }
 
-const agentSim = new AgentSimController({ netConnection: netConn })
+const agentSim = new AgentSimController({ trajectoryPlaybackFile: "ATPsynthase_9.h5", netConnection: netConn });
 let currentFrame = 0;
 let currentTime = 0;
-
-
 
 const intialState = {
     highlightId: -1,
