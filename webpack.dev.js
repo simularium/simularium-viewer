4d557e14e62a586b95073785b8cc6d6c74baf111
Dan <danielt@alleninstitute.org>
2020-07-09 16:22:14 -0700

    remove dead code

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,71 +1,74 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-//const WorkerPlugin = require("worker-plugin");
 
 module.exports = {
-    entry: './examples/index.tsx',
+    entry: "./examples/index.tsx",
     output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, 'public'),
+        filename: "index.js",
+        path: path.resolve(__dirname, "public"),
     },
-    mode: 'development',
-    devtool: 'inline-source-map',
+    mode: "development",
+    devtool: "inline-source-map",
     plugins: [
         new HtmlWebpackPlugin({
-            template: './examples/index.html'
+            template: "./examples/index.html",
         }),
         new MiniCssExtractPlugin({
-            filename: 'style.[contenthash].css'
+            filename: "style.[contenthash].css",
         }),
-        new CopyWebpackPlugin([{
-            from: 'examples/assets',
-            to: path.resolve(__dirname, 'public/assets')
-        }]),
+        new CopyWebpackPlugin([
+            {
+                from: "examples/assets",
+                to: path.resolve(__dirname, "public/assets"),
+            },
+        ]),
         //new WorkerPlugin()
     ],
     devServer: {
-        publicPath: '/public/',
-        openPage: 'public/',
-        disableHostCheck: true
+        publicPath: "/public/",
+        openPage: "public/",
+        disableHostCheck: true,
     },
     module: {
-        rules: [{
+        rules: [
+            {
                 test: /\.(j|t)sx?$/,
                 exclude: /node_modules/,
-                use: [{
-                    loader: 'babel-loader'
-                }, ],
+                use: [
+                    {
+                        loader: "babel-loader",
+                    },
+                ],
             },
             {
                 test: /\.css/,
-                include: [
-                    path.resolve(__dirname, "examples"),
-                ],
-                use: [{
-                        loader: MiniCssExtractPlugin.loader
+                include: [path.resolve(__dirname, "examples")],
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
                     },
                     {
-                        loader: "css-loader"
+                        loader: "css-loader",
                     },
                 ],
-
             },
             {
                 test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader'
-                ]
+                use: ["file-loader"],
             },
             {
                 test: /KMeansWorker\.(js|ts)$/i,
-                use: [{
-                    loader: "worker-loader?inline=true"
-                }, {
-                    loader: "babel-loader"
-                }]
-            }
+                use: [
+                    {
+                        loader: "worker-loader?inline=true",
+                    },
+                    {
+                        loader: "babel-loader",
+                    },
+                ],
+            },
         ],
     },
     resolve: {
