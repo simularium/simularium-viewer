e5fc0d181df58bc0a93789dc57a9456013a6ddbb
Dan <danielt@alleninstitute.org>
2020-10-30 12:48:00 -0700

    fix instance buffer updating and do some cleanup

--- src/simularium/rendering/InstancedFiberEndcaps.ts
+++ src/simularium/rendering/InstancedFiberEndcaps.ts
@@ -1,5 +1,4 @@
 import {
-    BufferAttribute,
     SphereBufferGeometry,
     InstancedBufferAttribute,
     InstancedBufferGeometry,
@@ -12,31 +11,40 @@ class InstancedFiberEndcaps implements IInstancedFiberEndcaps {
     private endcapGeometry: SphereBufferGeometry;
     private mesh: Mesh;
     private instancedGeometry: InstancedBufferGeometry;
-    private positionArray: Float32Array; // x,y,z,scale
-    private rotationArray: Float32Array; // quaternion xyzw
-    private instanceArray: Float32Array; // instance id, type id (color index)
+    private positionAttribute: InstancedBufferAttribute; // x,y,z,scale
+    private rotationAttribute: InstancedBufferAttribute; // quaternion xyzw
+    private instanceAttribute: InstancedBufferAttribute; // instance id, type id (color index)
     private currentInstance: number;
     private isUpdating: boolean;
 
     constructor() {
         this.mesh = new Mesh();
         this.instancedGeometry = new InstancedBufferGeometry();
-        this.positionArray = new Float32Array();
-        this.instanceArray = new Float32Array();
-        this.rotationArray = new Float32Array();
+        this.positionAttribute = new InstancedBufferAttribute(
+            new Float32Array(),
+            4,
+            false
+        );
+        this.rotationAttribute = new InstancedBufferAttribute(
+            new Float32Array(),
+            4,
+            false
+        );
+        this.instanceAttribute = new InstancedBufferAttribute(
+            new Float32Array(),
+            2,
+            false
+        );
+
         this.currentInstance = 0;
         this.isUpdating = false;
         this.endcapGeometry = new SphereBufferGeometry(1, 8, 8);
     }
 
     public create(n: number): void {
-        this.instancedGeometry = new InstancedBufferGeometry().copy(
-            this.endcapGeometry
-        );
-        const instanceCount = n;
-        this.instancedGeometry.instanceCount = instanceCount;
-        //let material = new THREE.ShaderMaterial();
-        this.mesh = new Mesh(this.instancedGeometry); //, material);
+        this.reallocate(n);
+        this.instancedGeometry.instanceCount = n;
+        this.mesh = new Mesh(this.instancedGeometry);
         this.mesh.frustumCulled = false;
     }
 
@@ -44,51 +52,67 @@ class InstancedFiberEndcaps implements IInstancedFiberEndcaps {
         return this.mesh;
     }
 
-    // send in new instance data
-    private updateInstanceBuffer(
-        attributeName: string,
-        data: Float32Array,
-        nfloatsPerValue = 3
-    ): void {
-        this.instancedGeometry.setAttribute(
-            attributeName,
-            new InstancedBufferAttribute(data, nfloatsPerValue, false)
-        );
-    }
-
     private updateInstanceCount(n: number): void {
+        //console.log("total draws = " + n);
         this.instancedGeometry.instanceCount = n;
     }
 
     private reallocate(n: number): void {
-        const newPos = new Float32Array(4 * n);
-        newPos.set(this.positionArray);
-        this.positionArray = newPos;
+        // tell threejs/webgl that we can discard the old buffers
+        this.instancedGeometry.dispose();
 
-        const newInst = new Float32Array(2 * n);
-        newInst.set(this.instanceArray);
-        this.instanceArray = newInst;
+        // we must create a new Geometry to have things update correctly
+        this.instancedGeometry = new InstancedBufferGeometry().copy(
+            this.endcapGeometry
+        );
+        // install the new geometry into our Mesh object
+        this.mesh.geometry = this.instancedGeometry;
+
+        // make new array,
+        // copy old array into it,
+        // reset into instancedGeometry
+
+        const newPos = new Float32Array(4 * n);
+        newPos.set(this.positionAttribute.array);
+        this.positionAttribute = new InstancedBufferAttribute(newPos, 4, false);
+        this.instancedGeometry.setAttribute(
+            "translateAndScale",
+            this.positionAttribute
+        );
 
         const newRot = new Float32Array(4 * n);
-        newRot.set(this.rotationArray);
-        this.rotationArray = newRot;
+        newRot.set(this.rotationAttribute.array);
+        this.rotationAttribute = new InstancedBufferAttribute(newRot, 4, false);
+        this.instancedGeometry.setAttribute(
+            "rotationQ",
+            this.rotationAttribute
+        );
 
-        this.updateInstanceBuffer("translateAndScale", this.positionArray, 4);
-        this.updateInstanceBuffer("rotationQ", this.rotationArray, 4);
-        this.updateInstanceBuffer("instanceAndTypeId", this.instanceArray, 2);
+        const newInst = new Float32Array(2 * n);
+        newInst.set(this.instanceAttribute.array);
+        this.instanceAttribute = new InstancedBufferAttribute(
+            newInst,
+            2,
+            false
+        );
+        this.instancedGeometry.setAttribute(
+            "instanceAndTypeId",
+            this.instanceAttribute
+        );
     }
 
     beginUpdate(nAgents: number): void {
         // do we need to increase storage?
         const increment = 4096;
-        const currentNumInstances = this.instanceArray.length / 2;
+        // total num instances possible in buffer
+        const currentNumInstances = this.instanceAttribute.count;
+        // num of instances needed
         const requestedNumInstances = nAgents * 2;
-        // two instances per agent.
+
         if (requestedNumInstances > currentNumInstances) {
-            // increase to next multiple of 4096 above nAgents
+            // increase to next multiple of increment
             const newInstanceCount =
                 (Math.trunc(requestedNumInstances / increment) + 1) * increment;
-            console.log("realloc to " + newInstanceCount + " instances");
 
             this.reallocate(newInstanceCount);
         }
@@ -108,19 +132,12 @@ class InstancedFiberEndcaps implements IInstancedFiberEndcaps {
         qw: number,
         instanceId: number,
         typeId: number,
-        c: Color
+        c: Color // eslint-disable-line @typescript-eslint/no-unused-vars
     ): void {
         const offset = this.currentInstance;
-        this.positionArray[offset * 4 + 0] = x;
-        this.positionArray[offset * 4 + 1] = y;
-        this.positionArray[offset * 4 + 2] = z;
-        this.positionArray[offset * 4 + 3] = scale;
-        this.instanceArray[offset * 2 + 0] = instanceId;
-        this.instanceArray[offset * 2 + 1] = typeId;
-        this.rotationArray[offset * 4 + 0] = qx;
-        this.rotationArray[offset * 4 + 1] = qy;
-        this.rotationArray[offset * 4 + 2] = qz;
-        this.rotationArray[offset * 4 + 3] = qw;
+        this.positionAttribute.setXYZW(offset, x, y, z, scale);
+        this.instanceAttribute.setXY(offset, instanceId, typeId);
+        this.rotationAttribute.setXYZW(offset, qx, qy, qz, qw);
 
         this.currentInstance++;
     }
@@ -129,16 +146,9 @@ class InstancedFiberEndcaps implements IInstancedFiberEndcaps {
         this.updateInstanceCount(this.currentInstance);
 
         // assumes the entire buffers are invalidated.
-        (this.instancedGeometry.getAttribute(
-            "translateAndScale"
-        ) as BufferAttribute).needsUpdate = true;
-        (this.instancedGeometry.getAttribute(
-            "rotationQ"
-        ) as BufferAttribute).needsUpdate = true;
-        (this.instancedGeometry.getAttribute(
-            "instanceAndTypeId"
-        ) as BufferAttribute).needsUpdate = true;
-
+        this.instanceAttribute.needsUpdate = true;
+        this.rotationAttribute.needsUpdate = true;
+        this.positionAttribute.needsUpdate = true;
         this.isUpdating = false;
     }
 }
