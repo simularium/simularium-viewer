c38e15633116a4fa6b573315eaeede39f7f27c0e
Na Hyung Choi <schoinh@gmail.com>
2020-12-02 14:53:03 -0800

    Add comment to viewport file

--- src/viewport/index.tsx
+++ src/viewport/index.tsx
@@ -209,6 +209,10 @@ class Viewport extends React.Component<ViewportProps, ViewportState> {
             msg: TrajectoryFileInfo
         ) => {
             this.visGeometry.handleTrajectoryData(msg);
+            // handleTrajectoryData() above creates a new bounding box and tick marks
+            // (via resetBounds()) and sets VisGeometry.tickIntervalLength, which is now
+            // available for use as the length of the scale bar in the UI.
+            simulariumController.tickIntervalLength = this.visGeometry.tickIntervalLength;
             try {
                 this.selectionInterface.parse(msg.typeMapping);
             } catch (e) {
@@ -218,7 +222,6 @@ class Viewport extends React.Component<ViewportProps, ViewportState> {
                     console.log("error parsing 'typeMapping' data", e);
                 }
             }
-            simulariumController.tickIntervalLength = this.visGeometry.tickIntervalLength;
             onTrajectoryFileInfoChanged(msg);
 
             const uiDisplayData = this.selectionInterface.getUIDisplayData();
