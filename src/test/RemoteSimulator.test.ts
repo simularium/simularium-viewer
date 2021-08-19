52903c395dae92977e6057e67feede30e32aa322
Na Hyung Choi <schoinh@gmail.com>
2021-08-19 16:19:58 -0700

    Remove simulatorParams const from tests

--- src/test/RemoteSimulator.test.ts
+++ src/test/RemoteSimulator.test.ts
@@ -1,18 +1,15 @@
-import { TEST_CONNECTION_SETTINGS } from "../constants";
-
-import { RemoteSimulator } from "..";
 import {
     CONNECTION_SUCCESS_MSG,
     CONNECTION_FAIL_MSG,
 } from "../simularium/RemoteSimulator";
 import FrontEndError from "../simularium/FrontEndError";
+import { TEST_CONNECTION_SETTINGS } from "../constants";
+import { RemoteSimulator } from "..";
 
 describe("RemoteSimulator", () => {
     // Silence console.debug messages like this in Jest output:
     // "[netconnection] WS Connection Request Sent:  wss://..."
     jest.spyOn(global.console, "debug").mockImplementation(() => jest.fn());
-
-    const simulatorParams = TEST_CONNECTION_SETTINGS;
 
     describe("connectToUri", () => {
         test("creates a valid WebSocket object", () => {
@@ -26,14 +23,14 @@ describe("RemoteSimulator", () => {
 
     describe("connectToRemoteServer", () => {
         test("successfully connects to server", async () => {
-            const simulator = new RemoteSimulator(simulatorParams);
+            const simulator = new RemoteSimulator(TEST_CONNECTION_SETTINGS);
             const message = await simulator.connectToRemoteServer(
                 simulator.getIp()
             );
             expect(message).toEqual(CONNECTION_SUCCESS_MSG);
         });
         test("emits error if connecting to server takes longer than allotted time", async () => {
-            const simulator = new RemoteSimulator(simulatorParams);
+            const simulator = new RemoteSimulator(TEST_CONNECTION_SETTINGS);
             const veryShortTimeout = 1;
 
             try {
@@ -49,7 +46,7 @@ describe("RemoteSimulator", () => {
 
     describe("startRemoteTrajectoryPlayback", () => {
         test("sends WebSocket request to start playback", async () => {
-            const simulator = new RemoteSimulator(simulatorParams);
+            const simulator = new RemoteSimulator(TEST_CONNECTION_SETTINGS);
             const request = jest.spyOn(simulator, "sendWebSocketRequest");
             expect(request).not.toBeCalled();
 
@@ -59,7 +56,7 @@ describe("RemoteSimulator", () => {
             expect(request).toBeCalled();
         });
         test("throws error emitted by connectToRemoteServer as a FrontEndError if connection fails", async () => {
-            const simulator = new RemoteSimulator(simulatorParams);
+            const simulator = new RemoteSimulator(TEST_CONNECTION_SETTINGS);
             jest.spyOn(simulator, "connectToRemoteServer").mockRejectedValue(
                 new Error("Mock error message")
             );
