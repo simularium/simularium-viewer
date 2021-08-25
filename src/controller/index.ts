d94f114247132d65827584e7d188865bbb073b0a
Na Hyung Choi <schoinh@gmail.com>
2021-08-24 17:54:57 -0700

    Bind this for all camera control methods

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -108,6 +108,14 @@ export default class SimulariumController {
             this.playBackFile
         );
         this.assetPrefix = params.assetLocation || DEFAULT_ASSET_PREFIX;
+
+        this.zoomIn = this.zoomIn.bind(this);
+        this.zoomOut = this.zoomOut.bind(this);
+        this.resetCamera = this.resetCamera.bind(this);
+        this.centerCamera = this.centerCamera.bind(this);
+        this.reOrientCamera = this.reOrientCamera.bind(this);
+        this.setPanningMode = this.setPanningMode.bind(this);
+        this.setFocusMode = this.setFocusMode.bind(this);
     }
 
     private resolveGeometryFile(
@@ -424,6 +432,7 @@ export default class SimulariumController {
     }
 
     public setPanningMode(pan: boolean): void {
+        console.log("THIS:", this);
         if (this.visGeometry) {
             this.visGeometry.setPanningMode(pan);
         }
