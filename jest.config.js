70984b5cecb68ce17f431066813cc20178c8d954
Dan <danielt@alleninstitute.org>
2021-09-07 07:51:14 -0700

    tests passing!

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,11 @@
 module.exports = {
     preset: "ts-jest",
-    testEnvironment: "jsdom",
-    // moduleFileExtensions: ["ts", "js", "tsx", "jsx"],
-    extensionsToTreatAsEsm: [".ts", ".tsx", ".jsx"],
-    globals: {
-        "ts-jest": {
-            tsconfig: "tsconfig.jest.json",
-            useESM: true,
-        },
-    },
     transform: {
-        "^.+\\.(ts|tsx|js|jsx)?$": "ts-jest",
-        // "^.+\\.(js|jsx)$": "babel-jest",
+        "^.+\\.(ts|tsx|js|jsx)$": ["esbuild-jest"],
     },
-    //  https://jestjs.io/docs/webpack#mocking-css-modules
+    moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
+    testEnvironment: "jsdom",
+    // From https://jestjs.io/docs/webpack#mocking-css-modules
     moduleNameMapper: {
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
             "<rootDir>/scripts/jestAssetTransformer.js",
