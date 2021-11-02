a006736975d0451b6b2ed6c94511c98ba69dd548
meganrm <meganrm@gmail.com>
2021-11-02 00:22:01 -0700

    add levels to front end error

--- src/simularium/FrontEndError.ts
+++ src/simularium/FrontEndError.ts
@@ -1,10 +1,17 @@
+export enum ErrorLevel {
+    ERROR = "error",
+    INFO = "info",
+    WARNING = "warning",
+}
+
 class FrontEndError extends Error {
     public htmlData: string;
-    constructor(htmlData = "", ...params: string[]) {
+    public level: ErrorLevel;
+    constructor(htmlData = "", level = ErrorLevel.ERROR, ...params: string[]) {
         // Pass remaining arguments (including vendor specific ones) to parent constructor
         super(...params);
-
         this.name = "FrontEndError";
+        this.level = level;
         this.htmlData = htmlData;
     }
 }
