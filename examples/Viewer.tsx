6ac5aa4d27599e2fba3140a88a4d27c69da25ba5
meganrm <meganrm@gmail.com>
2021-11-02 00:21:02 -0700

    add onError function for levels

--- examples/Viewer.tsx
+++ examples/Viewer.tsx
@@ -7,6 +7,7 @@ import type {
 import SimulariumViewer, {
     SimulariumController,
     RenderStyle,
+    ErrorLevel,
 } from "../src";
 import "./style.css";
 import { isEqual, findIndex } from "lodash";
@@ -14,6 +15,7 @@ import { isEqual, findIndex } from "lodash";
 import PointSimulator from "./PointSimulator";
 import PdbSimulator from "./PdbSimulator";
 import CurveSimulator from "./CurveSimulator";
+import FrontEndError from "../src/simularium/FrontEndError";
 
 const netConnectionSettings = {
     serverIp: "staging-node1-agentviz-backend.cellexplore.net",
@@ -134,6 +136,16 @@ class Viewer extends React.Component<{}, ViewerState> {
         e.preventDefault();
     };
 
+    public onError = (error: FrontEndError) => {
+        if (error.level === ErrorLevel.ERROR) {
+            window.alert(`ALERT, something is broken: ${error.htmlData}`);
+        } else if (error.level === ErrorLevel.WARNING) {
+            window.alert(`User warning, but not terrible: ${error.htmlData}`);
+        } else {
+            console.log(`Just for your info: ${error.htmlData}`);
+        }
+    };
+
     public onDrop = (e: Event): void => {
         this.onDragOver(e);
         const event = e as DragEvent;
@@ -150,7 +162,12 @@ class Viewer extends React.Component<{}, ViewerState> {
             const simulariumFileIndex = findIndex(filesArr, (file) =>
                 file.name.includes(".simularium")
             );
-            const simulariumFile = JSON.parse(parsedFiles[simulariumFileIndex]);
+            let simulariumFile;
+            try {
+                simulariumFile = JSON.parse(parsedFiles[simulariumFileIndex]);
+            } catch (error) {
+                return this.onError(new FrontEndError(error.message));
+            }
             const fileName = filesArr[simulariumFileIndex].name;
             const geoAssets = filesArr.reduce((acc, cur, index) => {
                 if (index !== simulariumFileIndex) {
@@ -413,13 +430,21 @@ class Viewer extends React.Component<{}, ViewerState> {
                             <label htmlFor={id}>{id}</label>
                             <input
                                 type="checkbox"
-                                onClick={(event) => this.turnAgentsOnOff((event.target as HTMLInputElement).value)}
+                                onClick={(event) =>
+                                    this.turnAgentsOnOff(
+                                        (event.target as HTMLInputElement).value
+                                    )
+                                }
                                 value={id}
                                 defaultChecked={true}
                             />
                             <input
                                 type="checkbox"
-                                onClick={(event) => this.turnAgentHighlightsOnOff((event.target as HTMLInputElement).value)}
+                                onClick={(event) =>
+                                    this.turnAgentHighlightsOnOff(
+                                        (event.target as HTMLInputElement).value
+                                    )
+                                }
                                 value={id}
                                 defaultChecked={false}
                             />
@@ -511,7 +536,7 @@ class Viewer extends React.Component<{}, ViewerState> {
                         agentColors={this.state.agentColors}
                         hideAllAgents={this.state.hideAllAgents}
                         showPaths={this.state.showPaths}
-                        onError={(error) => window.alert(error)}
+                        onError={this.onError}
                         backgroundColor={[0, 0, 0]}
                     />
                 </div>
