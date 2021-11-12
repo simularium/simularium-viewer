6b59252a08b303a122fd30fb2787aac4d4a69fd2
dmt <dmt@completelydifferent.com>
2021-11-11 21:31:06 -0800

    change keypress to ctrl+alt+G

--- src/viewport/index.tsx
+++ src/viewport/index.tsx
@@ -362,7 +362,7 @@ class Viewport extends React.Component<
         }
 
         const event = e as KeyboardEvent;
-        if (event.key === "f") {
+        if (event.key === "g" && event.altKey && event.ctrlKey) {
             const s = this.state.showRenderParamsGUI;
             this.setState({ showRenderParamsGUI: !s });
         }
