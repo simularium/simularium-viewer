b94f2af0a6a2b4f4d53a9924801b8846ac707821
toloudis <danielt@alleninstitute.org>
2022-02-16 20:14:53 -0800

    filling out some implementation for binary parser

--- src/simularium/BinaryFileParser.ts
+++ src/simularium/BinaryFileParser.ts
@@ -1,5 +1,6 @@
 import type ISimulariumFile from "./ISimulariumFile";
 import type { TrajectoryFileInfo, VisDataFrame } from "./types";
+import { compareTimes } from "../util";
 
 const enum BlockTypeEnum {
     // type = 0 : spatial data block in JSON
@@ -31,11 +32,40 @@ export default class BinaryFileReader implements ISimulariumFile {
     dataView: DataView;
     header: Header;
     tfi: TrajectoryFileInfo;
+    nFrames: number;
+    frameOffsets: number[];
+    spatialData: Float32Array;
     constructor(fileContents: ArrayBuffer) {
+        this.nFrames = 0;
+        this.frameOffsets = [];
         this.fileContents = fileContents;
         this.dataView = new DataView(fileContents);
         this.header = this.readHeader();
         this.tfi = this.getTrajectoryFileInfo();
+        this.spatialData = this.readSpatialDataInfo();
+    }
+
+    private readSpatialDataInfo(): Float32Array {
+        // find spatial data block and load frame offsets
+        for (const block of this.header.blocks) {
+            if (block.type === BlockTypeEnum.SPATIAL_DATA_BINARY) {
+                const blockData = this.getBlock(block);
+                // Spatial data version (4-byte int)
+                const version = blockData.getInt32(0, true);
+                // Number of frames (4-byte int)
+                this.nFrames = blockData.getInt32(4, true);
+                // Frame offsets (Number of frames * 4-byte int)
+                for (let i = 0; i < this.nFrames; i++) {
+                    this.frameOffsets.push(blockData.getInt32(8 + i * 4, true));
+                }
+                return new Float32Array(
+                    blockData.buffer,
+                    blockData.byteOffset,
+                    blockData.byteLength
+                );
+            }
+        }
+        throw new Error("No spatial data block found");
     }
 
     private readHeader(): Header {
@@ -90,7 +120,8 @@ export default class BinaryFileReader implements ISimulariumFile {
                     blockSize
             );
         }
-        // note: NOT a copy
+        // note: NOT a copy.
+        // never produce copies internally. let callers make a copy if they need it.
         // also note: return the contents of the block NOT INCLUDING the block header
         return new DataView(
             this.fileContents,
@@ -117,12 +148,34 @@ export default class BinaryFileReader implements ISimulariumFile {
     }
 
     getNumFrames(): number {
-        return 0;
+        return this.nFrames;
     }
+
     getFrameIndexAtTime(time: number): number {
-        return 0;
+        const { timeStepSize } = this.tfi;
+        // Find the index of the frame that has the time matching our target time:
+        // walk offsets and parse frame times.
+        // assumes frames are in ascending order by time!
+        for (let i = 0; i < this.nFrames; i++) {
+            // get time of frame.
+            const frameFloatOffset = this.frameOffsets[i] / 4;
+            const frameNumber = this.spatialData[frameFloatOffset + 0];
+            const frameTime = this.spatialData[frameFloatOffset + 1];
+            // check time
+            if (compareTimes(frameTime, time, timeStepSize) === 0) {
+                return i;
+            }
+        }
+
+        return -1;
     }
+
     getFrame(theFrameNumber: number): VisDataFrame | ArrayBuffer {
-        return {} as VisDataFrame;
+        const frameOffset = this.frameOffsets[theFrameNumber];
+        const totalOffset = this.spatialData.byteOffset + frameOffset;
+        const frameSize = this.spatialData[totalOffset / 4 + 0];
+        // return an arraybuffer copy?
+        const frameContents = this.fileContents.slice(totalOffset, frameSize);
+        return frameContents;
     }
 }
