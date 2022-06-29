699919a748ce49167d094a7ac5346809447003e6
meganrm <meganrm@gmail.com>
2022-06-28 19:04:39 -0700

    expand out base component

--- examples/ConversionForm/BaseInput.tsx
+++ examples/ConversionForm/BaseInput.tsx
@@ -1,8 +1,43 @@
 import React from "react";
 
-export default (props) => { 
-    console.log(props)
-    return (<label> {props.name}
-        <input type="text" onChange={props.handler}/>
-    </label>)
-}
+const BaseInput =  (props) => {
+
+    switch (props.dataType) {
+        case "string":
+            return (
+                <label>
+                    {" "}
+                    {props.name}
+                    <input type="text" onChange={props.handler} />
+                </label>
+            );
+        case "number":
+            return (
+                <label>
+                    {" "}
+                    {props.name}
+                    <input type="number" onChange={props.handler} />
+                </label>
+            );
+        case "enum":
+            return (
+                <select>
+                    {props.options.map((id) => (
+                        <option value={id}>{id}</option>
+                    ))}
+                </select>
+            );
+   
+            
+        default:
+            return (
+                <label>
+                    {" "}
+                    {props.name}
+                    <input type="text" onChange={props.handler} />
+                </label>
+            );
+    }
+};
+
+export default BaseInput;
