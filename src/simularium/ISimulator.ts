9b0fe38a109fe4ffe2fbd9747b9901d1995a3ee3
toloudis <danielt@alleninstitute.org>
2022-07-22 13:00:49 -0700

    grouping things

--- src/simularium/ISimulator.ts
+++ src/simularium/ISimulator.ts
@@ -15,32 +15,17 @@ export interface ISimulator {
         handler: (msg: VisDataMessage | ArrayBuffer) => void
     ): void;
 
-    socketIsValid(): boolean;
-
     /**
      * Connect
      * */
+    connectToRemoteServer(address: string): Promise<string>;
+    socketIsValid(): boolean;
+    getIp(): string;
     disconnect(): void;
 
-    getIp(): string;
-
-    connectToRemoteServer(address: string): Promise<string>;
-
+    // TODO Fold into sendUpdate
     sendTimeStepUpdate(newTimeStep: number): void;
-
-    // what are the things we want to change?
-    // rates?
-    // state vs parameter
-
-    // distinguish between construction only parameters ("parameters") and
-    // params you can change DURING the run ("state")
-
-    // some things require a restart if you change them.
-    // total number of agents
-
     sendUpdate(data: Record<string, unknown>): void;
-
-    sendModelDefinition(model: string): void;
 
     /**
      * Simulation Control
@@ -51,10 +36,15 @@ export interface ISimulator {
      *  Trajectory File: No simulation run, stream a result file piecemeal
      *
      */
-    // this should probably be abstracted to just "start"
+    // these should probably be abstracted to just "start"
+    // and have a separate interface for initialization?
+    // For example, "start" = initialize, and "resume" = play
     startRemoteSimPreRun(timeStep: number, numTimeSteps: number): void;
     startRemoteSimLive(): void;
     startRemoteTrajectoryPlayback(fileName: string): Promise<void>;
+    sendModelDefinition(model: string): void;
+
+    requestTrajectoryFileInfo(fileName: string): void;
 
     pauseRemoteSim(): void;
 
@@ -66,6 +56,4 @@ export interface ISimulator {
 
     // what do these mean for live mode?? can go backward but not forward
     gotoRemoteSimulationTime(time: number): void;
-
-    requestTrajectoryFileInfo(fileName: string): void;
 }
