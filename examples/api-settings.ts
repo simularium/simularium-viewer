c9be9e1e59de989efe91c916b5ec9e85f700758c
meganrm <meganrm@gmail.com>
2022-08-17 11:00:58 -0700

    update pointers now that branch is merged

--- examples/api-settings.ts
+++ examples/api-settings.ts
@@ -4,9 +4,9 @@ export const SMOLDYN_TEMPLATE = "smoldyn_data.json";
 export const UI_TEMPLATE_URL_ROOT =
     "https://api.github.com/repos/simularium/simulariumio/contents/ui-templates";
 
-export const UI_TEMPLATE_DOWNLOAD_URL_ROOT = "https://raw.githubusercontent.com/simularium/simulariumio/feature/ui-templates/ui-templates";
+export const UI_TEMPLATE_DOWNLOAD_URL_ROOT = "https://raw.githubusercontent.com/simularium/simulariumio/main/ui-templates";
 export const UI_BASE_TYPES = "base_types.json";
 
-export const UI_CUSTOM_TYPES = "custom-types?ref=feature/ui-templates";
+export const UI_CUSTOM_TYPES = "custom-types";
 
 
