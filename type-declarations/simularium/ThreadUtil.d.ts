ff308383a86188d5cbd1dd4592d4ff4672030763
Action: Push to Branch <action@github.com>
2022-12-01 19:24:19 +0000

    Update build to output generated at 08b7c51

--- type-declarations/simularium/ThreadUtil.d.ts
+++
@@ -1,6 +0,0 @@
-declare const threadUtil: {
-    browserSupportsWebWorkers(): boolean;
-    createWebWorkerFromFunction(fn: string): Worker;
-};
-export { threadUtil as ThreadUtil };
-export default threadUtil;
--- type-declarations/visGeometry/workers/KMeansWorker.d.ts
+++
@@ -1,6 +0,0 @@
-import "regenerator-runtime/runtime";
-declare class KMeansWorker {
-    run(k: any, sizes: any, data: any): Promise<Float32Array[]>;
-}
-export declare type KMeansWorkerType = typeof KMeansWorker;
-export {};
