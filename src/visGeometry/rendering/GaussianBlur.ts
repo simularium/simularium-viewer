d6875c24972a87258fb438d01a60651f67ff09c3
Daniel Toloudis <danielt@alleninstitute.org>
2023-05-17 14:52:18 -0700

    start defining a new blur implementation

--- src/visGeometry/rendering/GaussianBlur.ts
+++ src/visGeometry/rendering/GaussianBlur.ts
@@ -146,6 +146,148 @@ class BlurYPass {
     }
 }
 
+class BlurPass1D {
+    public pass: RenderToBuffer;
+
+    public constructor() {
+        this.pass = new RenderToBuffer({
+            defines: {
+                KERNEL_RADIUS: 4,
+                DEPTH_PACKING: 1,
+                PERSPECTIVE_CAMERA: 1,
+            },
+            uniforms: {
+                colorTex: { value: null },
+                size: { value: new Vector2(512, 512) },
+                sampleUvOffsets: { value: [new Vector2(0, 0)] },
+                sampleWeights: { value: [1.0] },
+                viewPosTex: { value: null },
+                // cameraNear: { value: 10 },
+                // cameraFar: { value: 1000 },
+                depthCutoff: { value: 10 }, // view space
+            },
+            fragmentShader: /* glsl */ `
+
+		#include <common>
+		#include <packing>
+
+        uniform sampler2D colorTex;
+        uniform sampler2D viewPosTex;
+
+//		uniform float cameraNear;
+//		uniform float cameraFar;
+		uniform float depthCutoff;
+
+		uniform vec2 sampleUvOffsets[ KERNEL_RADIUS + 1 ];
+		uniform float sampleWeights[ KERNEL_RADIUS + 1 ];
+
+		varying vec2 vUv;
+		varying vec2 vInvSize; // 1.0 / iResolution.xy
+
+		// float getDepth( const in vec2 screenPosition ) {
+		// 	#if DEPTH_PACKING == 1
+		// 	return unpackRGBAToDepth( texture2D( tDepth, screenPosition ) );
+		// 	#else
+		// 	return texture2D( tDepth, screenPosition ).x;
+		// 	#endif
+		// }
+
+		// float getViewZ( const in float depth ) {
+		// 	#if PERSPECTIVE_CAMERA == 1
+		// 	return perspectiveDepthToViewZ( depth, cameraNear, cameraFar );
+		// 	#else
+		// 	return orthographicDepthToViewZ( depth, cameraNear, cameraFar );
+		// 	#endif
+		// }
+
+		void main() {
+            vec4 viewPos4 = texture2D(viewPosTex, vUv);
+            if (viewPos4.w < 1.0) discard; 
+
+			float centerViewZ = -viewPos4.z;
+			bool rBreak = false, lBreak = false;
+
+			float weightSum = sampleWeights[0];
+			vec4 diffuseSum = texture2D( colorTex, vUv ) * weightSum;
+
+			for( int i = 1; i <= KERNEL_RADIUS; i ++ ) {
+
+				float sampleWeight = sampleWeights[i];
+				vec2 sampleUvOffset = sampleUvOffsets[i] * vInvSize;
+
+				vec2 sampleUv = vUv + sampleUvOffset;
+				float viewZ = -texture2D(viewPosTex, sampleUv).z;
+
+				if( abs( viewZ - centerViewZ ) > depthCutoff ) rBreak = true;
+
+				if( ! rBreak ) {
+					diffuseSum += texture2D( colorTex, sampleUv ) * sampleWeight;
+					weightSum += sampleWeight;
+				}
+
+				sampleUv = vUv - sampleUvOffset;
+				viewZ = -texture2D(viewPosTex, sampleUv).z;
+
+				if( abs( viewZ - centerViewZ ) > depthCutoff ) lBreak = true;
+
+				if( ! lBreak ) {
+					diffuseSum += texture2D( colorTex, sampleUv ) * sampleWeight;
+					weightSum += sampleWeight;
+				}
+
+			}
+
+			gl_FragColor = diffuseSum / weightSum;
+		}`,
+        });
+    }
+    public resize(x, y): void {
+        this.pass.material.uniforms.size.value = new Vector2(x, y);
+    }
+
+    public render(renderer, tgt): void {
+        this.pass.render(renderer, tgt);
+    }
+}
+
+function gaussian(x, stdDev) {
+    return (
+        Math.exp(-(x * x) / (2.0 * (stdDev * stdDev))) /
+        (Math.sqrt(2.0 * Math.PI) * stdDev)
+    );
+}
+
+const BlurShaderUtils = {
+    createSampleWeights: function (kernelRadius: number, stdDev: number) {
+        const weights: number[] = [];
+
+        for (let i = 0; i <= kernelRadius; i++) {
+            weights.push(gaussian(i, stdDev));
+        }
+
+        return weights;
+    },
+
+    createSampleOffsets: function (kernelRadius: number, uvIncrement: Vector2) {
+        const offsets: Vector2[] = [];
+
+        for (let i = 0; i <= kernelRadius; i++) {
+            offsets.push(uvIncrement.clone().multiplyScalar(i));
+        }
+
+        return offsets;
+    },
+
+    configure: function (material, kernelRadius, stdDev, uvIncrement) {
+        material.defines["KERNEL_RADIUS"] = kernelRadius;
+        material.uniforms["sampleUvOffsets"].value =
+            BlurShaderUtils.createSampleOffsets(kernelRadius, uvIncrement);
+        material.uniforms["sampleWeights"].value =
+            BlurShaderUtils.createSampleWeights(kernelRadius, stdDev);
+        material.needsUpdate = true;
+    },
+};
+
 class BlurPass {
     public blurXpass: BlurXPass;
     public blurYpass: BlurYPass;
