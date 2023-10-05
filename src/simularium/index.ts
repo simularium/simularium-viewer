a36f698c0c33edd3e96acc1e6ae1ed2a87d08cd6
Joe Heffernan <interim17@gmail.com>
2023-10-05 12:17:00 -0700

    typedeclarations colorchange and selectionentry

--- src/simularium/index.ts
+++ src/simularium/index.ts
@@ -6,7 +6,12 @@ export type {
     EncodedTypeMapping,
     SimulariumFileFormat,
 } from "./types";
-export type { SelectionStateInfo, UIDisplayData } from "./SelectionInterface";
+export type {
+    SelectionStateInfo,
+    UIDisplayData,
+    ColorChanges,
+    SelectionEntry,
+} from "./SelectionInterface";
 export { ErrorLevel, FrontEndError } from "./FrontEndError";
 export { NetMessageEnum } from "./WebsocketClient";
 export { RemoteSimulator } from "./RemoteSimulator";
