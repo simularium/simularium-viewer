5c1c84f1095c8b0b9efcafe5cabc1dd4c33ce6ca
Action: Push to Branch <action@github.com>
2023-12-05 00:06:00 +0000

    Update build to output generated at a6fd9fd

--- type-declarations/index.d.ts
+++ type-declarations/index.d.ts
@@ -1,6 +1,7 @@
 import Viewport from "./viewport";
 export type { SelectionStateInfo, UIDisplayData, VisDataFrame, ColorChanges, SelectionEntry, TrajectoryFileInfo, NetConnectionParams, } from "./simularium";
 export type { ISimulariumFile } from "./simularium/ISimulariumFile";
+export type { TimeData } from "./viewport";
 export { RenderStyle } from "./viewport";
 export { SimulariumController } from "./controller";
 export { Orchestrator, RemoteSimulator, ErrorLevel, FrontEndError, } from "./simularium";
