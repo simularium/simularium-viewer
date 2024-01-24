8b86f8aded21336440925eb6fddb80b9e567a8d3
Action: Push to Branch <action@github.com>
2024-01-24 22:31:06 +0000

    Update build to output generated at 4ff9c92

--- type-declarations/simularium/index.d.ts
+++ type-declarations/simularium/index.d.ts
@@ -1,5 +1,5 @@
 export type { NetConnectionParams } from "./WebsocketClient";
-export type { VisDataMessage, VisDataFrame, TrajectoryFileInfo, ModelInfo, EncodedTypeMapping, SimulariumFileFormat, } from "./types";
+export type { VisDataMessage, VisDataFrame, TrajectoryFileInfo, EncodedTypeMapping, SimulariumFileFormat, } from "./types";
 export type { SelectionStateInfo, UIDisplayData, SelectionEntry, ColorChange, } from "./SelectionInterface";
 export { ErrorLevel, FrontEndError } from "./FrontEndError";
 export { NetMessageEnum } from "./WebsocketClient";
