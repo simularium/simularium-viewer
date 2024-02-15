f102839f0b62fe5bf1d8ffcd345810508c5dc4b8
Joe Heffernan <interim17@gmail.com>
2024-02-15 11:02:59 -0800

    Feature/mp4 download (#344)
    * initial copy of nuc morph recorders
    
    * download button component
    
    * update tsconfig to work with web codecs
    
    * update dependencies to work w web codecs
    
    * track download button
    
    * remove old download button
    
    * first try to test streamrecorder
    
    * remove workshopping files
    
    * stream recorder class
    
    * download buttons component
    
    * video recorder ui component
    
    * add trajTitle to recorder
    
    * render video recorder in viewer
    
    * package.lock
    
    * pass down trajTitle for filename
    
    * specify fps in captureStream
    
    * add duration and status outputs
    
    * reorganize some lines and comments
    
    * remove unused prop
    
    * remove unused import
    
    * streamrecorder file extension fix
    
    * update comments
    
    * revert unneeded dependency changes, add types
    
    * remove unneeded ts config changes
    
    * comments on typeChecking
    
    * some cleanup, still typescript issue
    
    * export stream recorder from top level
    
    * move record movie component
    
    * export stream recorder from top level
    
    * declar custom canvas element type
    
    * initial draft of framerecorder
    
    * new implementation with by frame recording of render target
    
    * make new recorder instance on each set up
    
    * commit lock file
    
    * move download handling up to front end
    
    * add some error handling
    
    * add keyframes to mp4 at regular interval
    
    * move encoder instance comment to more relevant line
    
    * fix timestamp: divide frameindex by fps
    
    * function renaming, add clarifying comments and streamlined error handling
    
    * remove old streamrecorder and unused dom-media typing
    
    * add node to types in ts config
    
    * remove unused videoBlob from viewer state
    
    * don't call recorder methods for firefox users
    
    * remove unneeded "types" field from ts.config.base
    
    * define default frame rate constant
    
    * animate does not need to be async in this implementation
    
    * future check browser support by checking videoencoder in window
    
    * throw error when encoder config fails
    
    * remove unused state and check for browser compatibility in RecordMovieComponent
    
    * comment units on recording time and disable buttons if browser not supported
    
    * make browser supported a constant
    
    ---------
    
    Co-authored-by: meganrm <meganrm@gmail.com>

---
+++ examples/src/RecordMovieComponent.tsx
@@ -0,0 +1,70 @@
+import React, { useState, useEffect, useRef } from "react";
+
+interface RecordMovieComponentProps {
+    isRecording: boolean;
+    setIsRecording: (isRecording: boolean) => void;
+}
+
+const RecordMovieComponent = ({
+    isRecording,
+    setIsRecording,
+}: RecordMovieComponentProps) => {
+    // recording time measured in seconds
+    let [recordingTimeElapsed, setRecordingTimeElapsed] = useState<number>(0);
+    let [outputStatus, setOutputStatus] = useState<string>("");
+
+    const browserSupported = "VideoEncoder" in window;
+
+    // this useEffect is a timer that updates the recording duration
+    useEffect(() => {
+        let intervalId;
+        if (isRecording) {
+            intervalId = setInterval(() => {
+                setRecordingTimeElapsed((prevTimer) => prevTimer + 1);
+            }, 1000);
+        }
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, [isRecording]);
+
+    const startRecording = async () => {
+        setOutputStatus("");
+        setIsRecording(true);
+    };
+
+    const stopRecording = () => {
+        setOutputStatus("Recording complete");
+        setIsRecording(false);
+        setRecordingTimeElapsed(0);
+    };
+
+    return (
+        <div>
+            <button
+                onClick={startRecording}
+                disabled={isRecording || !browserSupported}
+            >
+                Start Recording
+            </button>
+            <button
+                onClick={stopRecording}
+                disabled={!isRecording || !browserSupported}
+            >
+                Stop Recording
+                </button>
+            <div>{isRecording ? "Recording..." : ""}</div>
+            <div> {!browserSupported ? "Browser does not support recording" : ""} </div>
+            <div>
+                {isRecording
+                    ? "Recording duration:  " +
+                      recordingTimeElapsed +
+                      " seconds"
+                    : ""}
+            </div>
+            <div>{outputStatus}</div>
+        </div>
+    );
+};
+
+export default RecordMovieComponent;
