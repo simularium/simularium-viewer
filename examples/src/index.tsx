67f16a2b10eaac0f5a8f4b973ee361894d501abc
Joe Heffernan <interim17@gmail.com>
2024-03-04 12:25:26 -0800

    use create root render method

--- examples/src/index.tsx
+++ examples/src/index.tsx
@@ -1,12 +1,17 @@
-import ReactDOM from "react-dom";
 import React from "react";
+import { createRoot } from "react-dom/client";
 
 import Viewer from './Viewer';
 
 declare const SIMULARIUM_USE_OCTOPUS: boolean;
 declare const SIMULARIUM_USE_LOCAL_BACKEND: boolean;
 
-ReactDOM.render(
-        <Viewer useOctopus={SIMULARIUM_USE_OCTOPUS} localBackendServer={SIMULARIUM_USE_LOCAL_BACKEND}/>,
-    document.getElementById("root")
+const container: HTMLElement | null = document.getElementById("root");
+
+const root = createRoot(container!);
+root.render(
+    <Viewer
+        useOctopus={SIMULARIUM_USE_OCTOPUS}
+        localBackendServer={SIMULARIUM_USE_LOCAL_BACKEND}
+    />
 );
