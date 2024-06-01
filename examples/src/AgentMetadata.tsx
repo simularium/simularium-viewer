83f7c0f54c9100a00a965f35915b82a2e72fe489
Joe Heffernan <interim17@gmail.com>
2024-05-31 17:35:11 -0700

    create metadata component for test bed

---
+++ examples/src/AgentMetadata.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { AgentData } from "../../type-declarations/simularium/types";
+
+interface AgentMetadataProps {
+    agentData: AgentData;
+}
+
+const AgentMetadata = ({ agentData }: AgentMetadataProps): JSX.Element => {
+
+    const getContents = () => {
+        if (agentData.instanceId === -1) {
+            return <div>No agent selected</div>;
+        }
+        return (
+            <div>
+                <div> uniqueID: {agentData.instanceId}</div>
+                <div> agentType: {agentData.type}</div>
+                <div>
+                    position: x = {agentData.x}, y = {agentData.y}, z =
+                    {agentData.z}
+                </div>
+                <div>
+                    rotation: x = {agentData.xrot}, y = {agentData.yrot}, z =
+                    {agentData.zrot}
+                </div>
+                <div> radius: {agentData.cr}</div>
+            </div>
+        );
+    };
+
+    return <div>Agent Metadata: {getContents()}</div>;
+};
+
+export default AgentMetadata;
