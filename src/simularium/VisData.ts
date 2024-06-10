06a852bfc0e2e53e0991a843a8b70744e5ca746f
Joe Heffernan <interim17@gmail.com>
2024-06-10 16:28:49 -0700

    spread null agent object when parsing frames

--- src/simularium/VisData.ts
+++ src/simularium/VisData.ts
@@ -49,7 +49,7 @@ class VisData {
         let j = AGENTS_OFFSET;
         for (let i = 0; i < expectedNumAgents; i++) {
             //TODO use visType in AgentData and convert from "vis-type" here at parse time
-            const agentData: AgentData = NULL_AGENT;
+            const agentData: AgentData = { ...NULL_AGENT };
 
             for (let k = 0; k < AGENT_OBJECT_KEYS.length; ++k) {
                 agentData[AGENT_OBJECT_KEYS[k]] = floatView[j++];
