5e1406622c2773d998465c4612b378133289de07
Cameron Fraser <cameron.fraser@alleninstitute.org>
2024-10-08 13:00:13 -0700

    add volume object w/ observer connection to drawable

--- src/visGeometry/VolumeModel.ts
+++ src/visGeometry/VolumeModel.ts
@@ -14,27 +14,48 @@ export default class VolumeModel {
     // TODO what to do with this `cancelled` property? Type check fails without it.
     // When should it be set, if ever; what should it be used for, if anything?
     public cancelled = false;
-    private image?: VolumeDrawable;
+    private drawable?: VolumeDrawable;
+    private volume?: Volume;
 
     public setImage(volumeObject: Volume): void {
-        this.image = new VolumeDrawable(volumeObject, {});
+        this.volume = volumeObject;
+        this.drawable = new VolumeDrawable(this.volume, {});
+        this.volume.addVolumeDataObserver(this);
     }
 
     public setAgentData(data: AgentData): void {
-        if (this.image) {
-            this.image.setTranslation(new Vector3(data.x, data.y, data.z));
-            this.image.setRotation(new Euler(data.xrot, data.yrot, data.zrot));
+        if (this.drawable) {
+            this.drawable.setTranslation(new Vector3(data.x, data.y, data.z));
+            this.drawable.setRotation(
+                new Euler(data.xrot, data.yrot, data.zrot)
+            );
             const r = data.cr * 2;
-            this.image.setScale(new Vector3(r, r, r));
+            this.drawable.setScale(new Vector3(r, r, r));
         }
     }
 
     public getObject3D(): Object3D | undefined {
-        return this.image?.sceneRoot;
+        return this.drawable?.sceneRoot;
     }
 
     public tempGetBoundingBoxObject(): Box3Helper | undefined {
-        this.image?.setShowBoundingBox(true);
-        return (this.image?.volumeRendering as TempRayMarchedVolume)?.boxHelper;
+        this.drawable?.setShowBoundingBox(true);
+        return (this.drawable?.volumeRendering as TempRayMarchedVolume)
+            ?.boxHelper;
+    }
+
+    // METHODS FROM `VolumeDataObserver` in volume-viewer
+
+    onVolumeData(_volume: Volume, channels: number[]): void {
+        this.drawable?.updateScale();
+        this.drawable?.onChannelLoaded(channels);
+    }
+
+    onVolumeChannelAdded(_volume: Volume, newChannelIndex: number): void {
+        this.drawable?.onChannelAdded(newChannelIndex);
+    }
+
+    onVolumeLoadError(_volume: Volume, error: unknown): void {
+        console.error("Volume load error", error);
     }
 }
