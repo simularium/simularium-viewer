3711c9e6e44b8c8c26e0b5d0799c00f39c595c4b
Cameron Fraser <cameron.fraser@alleninstitute.org>
2024-10-09 17:46:30 -0700

    lots of new methods, code to try n get these dang volumes to show up

--- src/visGeometry/VolumeModel.ts
+++ src/visGeometry/VolumeModel.ts
@@ -1,6 +1,11 @@
-import { Box3Helper, Euler, Object3D, Vector3 } from "three";
+import { Euler, Object3D, Vector3 } from "three";
 
-import { Volume, VolumeDrawable } from "@aics/volume-viewer";
+import {
+    Lut,
+    Volume,
+    VolumeDrawable,
+    HasThreeJsContext,
+} from "@aics/volume-viewer";
 
 import { AgentData } from "../simularium/types";
 
@@ -10,15 +15,31 @@ export default class VolumeModel {
     public cancelled = false;
     private drawable?: VolumeDrawable;
     private volume?: Volume;
+    private channelsEnabled: boolean[] = [];
+
+    private setEnabledChannels(channels: number[]): void {
+        if (!this.volume || !this.drawable) {
+            this.channelsEnabled = [];
+            return;
+        }
+        const { numChannels } = this.volume.imageInfo;
+        this.channelsEnabled = new Array(numChannels).fill(false);
+
+        for (const channel of channels) {
+            if (channel < numChannels) {
+                this.channelsEnabled[channel] = true;
+            }
+        }
+
+        for (const [channelIndex, enabled] of this.channelsEnabled.entries()) {
+            this.drawable.setVolumeChannelEnabled(channelIndex, enabled);
+        }
+    }
 
     public setImage(volumeObject: Volume): void {
         this.volume = volumeObject;
         this.drawable = new VolumeDrawable(this.volume, {});
         this.volume.addVolumeDataObserver(this);
-    }
-
-    public loadInitialData(): void {
-        this.volume?.loader?.loadVolumeData(this.volume);
     }
 
     public setAgentData(data: AgentData): void {
@@ -29,33 +50,74 @@ export default class VolumeModel {
             );
             const r = data.cr * 2;
             this.drawable.setScale(new Vector3(r, r, r));
-            // Volume agent data may use subpoint 0 as time
-            const time = data.subpoints?.length > 0 ? data.subpoints[0] : 0;
-            if (this.volume && this.volume.loadSpec.time !== time) {
-                this.volume.updateRequiredData({ time });
+            // Always defined if `drawable` is, but ts doesn't know that.
+            if (this.volume) {
+                // Volume agent data may use subpoint 0 as time
+                const numPoints = data.subpoints.length;
+                const time = numPoints > 0 ? data.subpoints[0] : 0;
+                if (this.volume.loadSpec.time !== time) {
+                    this.volume.updateRequiredData({ time });
+                }
+                // If there are more subpoints, they are enabled channel idxes.
+                // Otherwise, just channel 0 is enabled.
+                const channels = numPoints > 1 ? data.subpoints.slice(1) : [0];
+                this.setEnabledChannels(channels);
             }
         }
     }
 
+    public loadInitialData(): void {
+        this.volume?.loader?.loadVolumeData(this.volume);
+    }
+
     public getObject3D(): Object3D | undefined {
         // TODO showing bounding box for debugging purposes.
-        //   Do we want it on by default in production?
+        //   Do we want it on by default in production? Probably not?
         this.drawable?.setShowBoundingBox(true);
         return this.drawable?.sceneRoot;
     }
 
+    public onChannelLoaded(vol: Volume, channelIndex: number): void {
+        if (this.drawable) {
+            console.log("load", channelIndex);
+            const isEnabled = this.channelsEnabled[channelIndex];
+            this.drawable.setVolumeChannelEnabled(channelIndex, isEnabled);
+            this.drawable.updateScale();
+            this.drawable.onChannelLoaded([channelIndex]);
+            if (this.volume) {
+                const histo = this.volume.getHistogram(channelIndex);
+                const min = histo.findBinOfPercentile(0.5);
+                const max = histo.findBinOfPercentile(0.983);
+                const lut = new Lut().createFromMinMax(min, max);
+                this.volume.setLut(channelIndex, lut);
+            }
+            this.drawable.updateLuts();
+            this.drawable.fuse();
+            if (this.drawable && channelIndex === 0) {
+                console.log(this.drawable);
+            }
+        }
+    }
+
+    public onBeforeRender(context: HasThreeJsContext): void {
+        this.drawable?.onAnimate(context);
+    }
+
     // METHODS FROM `VolumeDataObserver` in volume-viewer
 
-    onVolumeData(_volume: Volume, channels: number[]): void {
+    public onVolumeData(_volume: Volume, channels: number[]): void {
         this.drawable?.updateScale();
         this.drawable?.onChannelLoaded(channels);
     }
 
-    onVolumeChannelAdded(_volume: Volume, newChannelIndex: number): void {
+    public onVolumeChannelAdded(
+        _volume: Volume,
+        newChannelIndex: number
+    ): void {
         this.drawable?.onChannelAdded(newChannelIndex);
     }
 
-    onVolumeLoadError(_volume: Volume, error: unknown): void {
+    public onVolumeLoadError(_volume: Volume, error: unknown): void {
         console.error("Volume load error", error);
     }
 }
