a9d472366530847a57f10d88736fe3002f1d24ec
Dan Toloudis <danielt@alleninstitute.org>
2024-11-21 14:42:08 -0800

    fix typecheck

--- src/test/AgentSimController.test.ts
+++ src/test/AgentSimController.test.ts
@@ -3,25 +3,26 @@ import { DummyRemoteSimulator } from "./DummyRemoteSimulator";
 
 describe("SimulariumController module", () => {
     describe("SimulariumController Time", () => {
-        test("Go to time in cache", (done) => {
-            const netConn = new DummyRemoteSimulator({});
-            netConn.timeStep = 1;
-            netConn.totalDuration = 100;
-            netConn.commandLatencyMS = 0;
-            netConn.connectLatencyMS = 0;
+        test("Go to time in cache", () =>
+            new Promise<void>((done) => {
+                const netConn = new DummyRemoteSimulator({});
+                netConn.timeStep = 1;
+                netConn.totalDuration = 100;
+                netConn.commandLatencyMS = 0;
+                netConn.connectLatencyMS = 0;
 
-            const controller = new SimulariumController({
-                remoteSimulator: netConn,
-            });
+                const controller = new SimulariumController({
+                    remoteSimulator: netConn,
+                });
 
-            controller.start();
-
-            // allow time for data streaming to occur
-            setTimeout(() => {
+                controller.start();
                 controller.gotoTime(2);
-                expect(controller.time()).toEqual(2);
-                done();
-            }, 500);
-        });
+
+                // allow time for data streaming to occur
+                setTimeout(() => {
+                    expect(controller.time()).toEqual(2);
+                    done();
+                }, 500);
+            }));
     });
 });
