208a89f297715f0b3a146ef702bb2b6f16c3eef6
Joe Heffernan <interim17@gmail.com>
2024-12-27 19:00:07 -0800

    fix autoconversion in test bed

--- examples/src/Viewer.tsx
+++ examples/src/Viewer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { isEqual, findIndex, reduce } from "lodash";
+import { isEqual, findIndex, reduce, map as lodashMap } from "lodash";
 import { v4 as uuidv4 } from "uuid";
 import { InputParams } from "tweakpane";
 
@@ -87,6 +87,8 @@ interface ViewerState {
     initialPlay: boolean;
     firstFrameTime: number;
     followObjectData: AgentData;
+    conversionActive: boolean;
+    conversionFileName: string;
 }
 
 const simulariumController = new SimulariumController({});
@@ -120,6 +122,8 @@ const initialState: ViewerState = {
     initialPlay: true,
     firstFrameTime: 0,
     followObjectData: nullAgent(),
+    conversionActive: false,
+    conversionFileName: "",
 };
 
 class Viewer extends React.Component<InputParams, ViewerState> {
@@ -284,7 +288,7 @@ class Viewer extends React.Component<InputParams, ViewerState> {
             .then((data) => data.json())
             .then((fileRefs) =>
                 Promise.all(
-                    map(fileRefs, (ref) =>
+                    lodashMap(fileRefs, (ref) =>
                         fetch(ref.download_url).then((data) => data.json())
                     )
                 )
@@ -333,6 +337,11 @@ class Viewer extends React.Component<InputParams, ViewerState> {
 
     public convertFile(obj: Record<string, any>, fileType: TrajectoryType) {
         const fileName = uuidv4() + ".simularium";
+        this.setState({
+            conversionActive: true,
+            conversionFileName: fileName,
+        });
+
         simulariumController
             .convertTrajectory(
                 this.netConnectionSettings,
@@ -342,13 +351,6 @@ class Viewer extends React.Component<InputParams, ViewerState> {
             )
             .then(() => {
                 this.clearPendingFile();
-            })
-            .then(() => {
-                simulariumController.changeFile(
-                    { netConnectionSettings: this.netConnectionSettings },
-                    fileName,
-                    true
-                );
             })
             .catch((err) => {
                 console.error(err);
@@ -430,8 +432,26 @@ class Viewer extends React.Component<InputParams, ViewerState> {
         });
     }
 
+    public receiveConvertedFile(): void {
+        simulariumController
+            .changeFile({
+                netConnectionSettings: this.netConnectionSettings,
+                
+            }, this.state.conversionFileName)
+            .then(() => {
+                simulariumController.gotoTime(0);
+            })
+            .then(() => this.setState({ conversionActive: false, conversionFileName: "" }))
+            .catch((e) => {
+                console.warn(e);
+            });
+    }
+
     public handleTrajectoryInfo(data: TrajectoryFileInfo): void {
         console.log("Trajectory info arrived", data);
+        if (this.state.conversionActive === true) {
+            this.receiveConvertedFile();
+        }
         // NOTE: Currently incorrectly assumes initial time of 0
         const totalDuration = (data.totalSteps - 1) * data.timeStepSize;
         this.setState({
