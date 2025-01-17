b005cf1a2b1411d44c3778f063cc229a1affec21
Joe Heffernan <interim17@gmail.com>
2025-01-17 13:35:37 -0800

    remove commented global variable declaration

--- examples/src/index.tsx
+++ examples/src/index.tsx
@@ -3,8 +3,6 @@ import { createRoot } from "react-dom/client";
 
 import Viewer from './Viewer';
 
-//declare const SIMULARIUM_USE_LOCAL_BACKEND: boolean;
-
 const container: HTMLElement | null = document.getElementById("root");
 
 const root = createRoot(container!);
