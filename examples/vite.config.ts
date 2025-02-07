efc33ea679cc7f4bbf274f6fcc49c658464c2482
Joe Heffernan <interim17@gmail.com>
2025-02-07 08:31:14 -0800

    watch mode for HMR (#444)
    * what if the test app used vite
    
    * try serving example
    
    * less config
    
    * update package-lock
    
    * apply vite config in start script and use port 8080
    
    * update lint config to allow numeric string properties
    
    * remove commented global variable declaration
    
    * try new eslint rule to allow numeric properties
    
    * use module export for local server global variable
    
    * generate types in gh actions and vite and tsconfig in examples to check the type declarations files
    
    * watch mode for HMR
    
    * fix output dir for css
    
    * consolidate watch scripts
    
    * restore script naming
    
    * dont run build in start script
    
    * run watch mode by default
    
    ---------
    
    Co-authored-by: dmt <dmt@completelydifferent.com>
    Co-authored-by: toloudis <danielt@alleninstitute.org>

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -1,5 +1,6 @@
 import type { UserConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default {
     plugins: [react()],
@@ -10,12 +11,18 @@ export default {
     },
     resolve: {
         dedupe: ["react", "react-dom"],
+        alias: {
+            "@aics/simularium-viewer": path.resolve(__dirname, "..", "es"),
+        },
     },
     optimizeDeps: {
-        include: ["@aics/simularium-viewer"],
+        include: [],
     },
     server: {
-        open: "/",
+        open: "examples/src/index.html",
         port: 8080,
+        fs: {
+            allow: [".."],
+        },
     },
 } satisfies UserConfig;
