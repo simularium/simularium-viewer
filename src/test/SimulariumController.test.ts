19fc1b7a485d2202f565dfc3cf8c80e09573f058
Joe Heffernan <interim17@gmail.com>
2025-03-17 16:15:15 -0700

    fix placeholder test

--- src/test/SimulariumController.test.ts
+++ src/test/SimulariumController.test.ts
@@ -1,10 +1,12 @@
+import SimulariumController from "../controller";
+
 describe("SimulariumController module", () => {
     describe("SimulariumController Time", () => {
-        test("Go to time in cache", () =>
-            new Promise<void>(() => {
-                // todo new approach to testing dummy simulator comming in
-                // changes to simulator params
-                return Promise.resolve();
-            }));
+        test("Go to time in cache", () => {
+            // todo new approach to testing dummy simulator comming in
+            // changes to simulator params
+            const controller = new SimulariumController();
+            expect(controller).toBeDefined();
+        });
     });
 });
