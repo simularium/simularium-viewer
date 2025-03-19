b82c27ba605a0921dba52edbed903f62fced9b88
Joe Heffernan <interim17@gmail.com>
2025-03-19 12:55:37 -0700

    remove redundant fragment

--- examples/src/Components/CacheLogDisplay.tsx
+++ examples/src/Components/CacheLogDisplay.tsx
@@ -16,19 +16,17 @@ const CacheLogDisplay: React.FC<CacheLogDisplayProps> = ({
 
     return (
         <div className="ui-container">
-            <>
-                <div>Cache Enabled: {cacheEnabled ? "Yes" : "No"}</div>
-                <div>Current cache size: {size}</div>
-                <div> Max Size: {maxSize}</div>
-                <div>First Frame Number: {framesInCache[0]}</div>
-                <div>
-                    Last Frame Number: {framesInCache[framesInCache.length - 1]}
-                </div>
-                <div>
-                    {framesInCache.length} frames in Cache:{" "}
-                    {framesInCache.join(", ")}
-                </div>
-            </>
+            <div>Cache Enabled: {cacheEnabled ? "Yes" : "No"}</div>
+            <div>Current cache size: {size}</div>
+            <div> Max Size: {maxSize}</div>
+            <div>First Frame Number: {framesInCache[0]}</div>
+            <div>
+                Last Frame Number: {framesInCache[framesInCache.length - 1]}
+            </div>
+            <div>
+                {framesInCache.length} frames in Cache:{" "}
+                {framesInCache.join(", ")}
+            </div>
         </div>
     );
 };
