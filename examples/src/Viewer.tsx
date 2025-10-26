544b5042212389f8f2eef7cf9984b4b3a640ff20
dmt <danielt@alleninstitute.org>
2025-10-25 18:45:39 -0700

    fix macos example app

--- examples/src/Viewer.tsx
+++ examples/src/Viewer.tsx
@@ -24,7 +24,7 @@ import type {
     IClientSimulatorImpl,
     CacheLog,
 } from "@aics/simularium-viewer";
-import "../../style/style.css";
+import "../../es/style/style.css";
 
 // local test bed imports
 import PointSimulator from "./simulators/PointSimulator.ts";
