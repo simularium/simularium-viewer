5984a1014fe50c78d050d187270e092ff2b67d66
Cameron Fraser <cameron.fraser@alleninstitute.org>
2026-05-04 17:09:20 -0700

    remove some commented-out code

--- src/test/testSetup.ts
+++ src/test/testSetup.ts
@@ -1,10 +1,3 @@
 // to be loaded by jest config's setupFiles
 import { Blob as CustomBlob } from "blob-polyfill";
 Blob = CustomBlob;
-// import {
-//     TextDecoder as CustomTextDecoder,
-//     TextEncoder as CustomTextEncoder,
-// } from "util";
-// /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-// TextDecoder = CustomTextDecoder;
-// TextEncoder = CustomTextEncoder;
